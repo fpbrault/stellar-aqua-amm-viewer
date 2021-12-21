d34190aa2fcead44dfc099081d95a011b939acdf
Félix Perron-Brault <fpbrault@gmail.com>
2021-12-21 13:52:04 -0500

    feat: 🎸 Use only one request to get reward data

--- pages/api/rewards.tsx
+++ pages/api/rewards.tsx
@@ -4,10 +4,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   res.setHeader("Cache-Control", "s-maxage=300");
 
   if (req.method === "GET") {
-    const resultsPage1 = await fetch("https://reward-api.aqua.network/api/rewards/?page=" + 1, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" }
-    })
+    const resultsPage1 = await fetch(
+      "https://reward-api.aqua.network/api/rewards/?page=1&page_size=100&ordering=-daily_total_reward",
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      }
+    )
       .then((response) => {
         if (response.status >= 400 && response.status < 600) {
           throw new Error(response.statusText);
@@ -17,35 +20,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       .catch((error) => {
         console.error(error);
       });
-    const resultsPage2 = await fetch("https://reward-api.aqua.network/api/rewards/?page=" + 2, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" }
-    })
-      .then((response) => {
-        if (response.status >= 400 && response.status < 600) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    const resultsPage3 = await fetch("https://reward-api.aqua.network/api/rewards/?page=" + 3, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" }
-    })
-      .then((response) => {
-        if (response.status >= 400 && response.status < 600) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+
     res.status(200);
 
-    res.json(resultsPage1.results.concat(resultsPage2.results, resultsPage3.results));
+    res.json(resultsPage1.results);
   } else {
     throw new Error(`The HTTP ${req.method} method is not supported at this route.`);
   }

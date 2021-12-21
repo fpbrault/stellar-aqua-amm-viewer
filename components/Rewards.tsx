28ea4259cb28955988b2f66819bd0c20b861db3d
Félix Perron-Brault <fpbrault@gmail.com>
2021-12-21 16:17:07 -0500

    fix: 🐛 fixes account data something missing

--- components/Rewards.tsx
+++ components/Rewards.tsx
@@ -155,10 +155,6 @@ const Rewards: React.FC = () => {
   }
 
   function handleRefreshData() {
-    mutate(
-      publicKey.length === 56 ? "https://horizon.stellar.org/accounts/" + publicKey : null,
-      false
-    );
     mutate(poolIds ? ["/api/pools", { pools: poolIds }] : null, false);
   }
 

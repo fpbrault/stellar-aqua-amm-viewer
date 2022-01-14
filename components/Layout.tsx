2cd4025643dab23b34cdaee007153098ec54fcea
Félix Perron-Brault <fpbrault@gmail.com>
2022-01-14 13:43:21 -0500

    feat: 🎸 updated favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -24,16 +24,16 @@ const Layout: React.FC<Props> = (props) => (
       <meta property="og:type" content="website" />
       <meta property="og:title" content={props.pageTitle} key="ogtitle" />
       <meta property="og:description" content="AMM AQUA rewards viewer" key="ogdesc" />
-      <meta
+      {/*       <meta
         property="og:image"
         content="https://upload.wikimedia.org/wikipedia/commons/5/56/Stellar_Symbol.png"
-      />
+      /> */}
       <meta property="twitter:domain" content="amm.stellar.beign.es" />
       <meta property="twitter:url" content="https://amm.stellar.beign.es/" />
-      <meta
+      {/*       <meta
         name="twitter:image"
         content="https://upload.wikimedia.org/wikipedia/commons/5/56/Stellar_Symbol.png"
-      />
+      /> */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={props.pageTitle} />
       <meta name="twitter:description" content="AMM AQUA rewards viewer" />
@@ -41,10 +41,14 @@ const Layout: React.FC<Props> = (props) => (
       <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#9b3eff" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      <meta name="theme-color" content="#317EFB" />
     </Head>
     <div className="flex flex-col text-neutral bg-base-200">
       <div className="min-h-screen pt-0">{props.children}</div>
